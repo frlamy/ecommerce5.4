0d4a422d0e93ac72105b982c0b0a1ad090231788
Franck Lamy <franck.lamy@neweb.com>
2023-02-03 15:30:12 +0100

    Add tax custom service

--- src/Controller/Front/HomepageController.php
+++ src/Controller/Front/HomepageController.php
@@ -2,12 +2,20 @@
 
 namespace App\Controller\Front;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomepageController extends AbstractController
 {
+    protected $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     /**
      * @Route("/", name="homepage")
      */
